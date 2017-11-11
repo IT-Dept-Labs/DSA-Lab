581b7d012cc4e7db82fb2ebee6a80cfe8b40ce04
Tilak N Shenoy <tilaknshenoy@gmail.com>
2017-11-11 13:24:36 +0530

    lab4

--- lab4/hashing.py
+++ lab4/hashing.py
@@ -7,72 +7,73 @@ class ListNode:
 
 
 class HashMap:
-    arr=[]
+	def __init__(self):
+		self.arr=[None]*30
 
-    def componentSum(self, string):
-        s = 0
-        for i in range(len(string)):
-            string.split()
-            s+=ord(string[i])
-        return s
+	def componentSum(self, string):
+		s = 0
+		for i in range(len(string)):
+			string.split()
+			s+=ord(string[i])
+		return s
 
-    def compressionMap(self,string):
-        res=self.componentSum(string)
-        return res%30
+	def compressionMap(self,string):
+		res=self.componentSum(string)
+		return res%30
 
-    def insertKey(self,string):
-        k=self.compressionMap(string)
+	def insertKey(self,string):
+		k=self.compressionMap(string)
 
-        if self.arr[k]==None:
-            n=ListNode()
-            n.val=string
-            self.arr[k]=n
+		if self.arr[k]==None:
+			n=ListNode()
+			n.val=string
+			self.arr[k]=n
 
-        else:
-            n1=ListNode()
-            n1.val=string
-            n1.nxt=self.arr[k]
-            self.arr[k]=n1
+		else:
+			n1=ListNode()
+			n1.val=string
+			n1.nxt=self.arr[k]
+			self.arr[k]=n1
 
-    def search(self,key):
-        res=self.compressionMap(key)
-        if self.arr[res]==None:
-            print('Not found')
-            return
-        elif self.arr[res].val==key:
-            print('Found')
-        elif self.arr[res].nxt!=None:
-            node=self.arr[res].nxt
-            while node!=None:
-                if node.val==key:
-                    print('Found')
-                    break
-                else:
-                    node=node.nxt
-            else:
-                print('Not found')
+	def search(self,key):
+		res=self.compressionMap(key)
+		if self.arr[res]==None:
+			print('Not found')
+			return
+		elif self.arr[res].val==key:
+			print('Found')
+		elif self.arr[res].nxt!=None:
+			node=self.arr[res].nxt
+			while node!=None:
+				if node.val==key:
+					print('Found')
+					break
+				else:
+					node=node.nxt
+			else:
+				print('Not found')
 
-    def keys(self):
-        for i in range(len(self.arr)):
-            if self.arr[i]!=None:
-                print(self.arr[i].val)
-                node = self.arr[i].nxt
-                while node != None:
-                    print(node.val)
-                    node=node.nxt
+	def keys(self):
+		for i in range(len(self.arr)):
+			if self.arr[i]!=None:
+				print(self.arr[i].val)
+				node = self.arr[i].nxt
+				while node != None:
+					print(node.val)
+					node=node.nxt
 
 def main():
-    H=HashMap()
-    print("Enter q to quit")
-    i=input("Enter the value: ")
-    while i!='q':
-        H.insertKey(i)
-        i=input('Enter the value: ')
-    search=input('Enter the search element: ')
-    H.search(search)
-    H.keys()
+	H=HashMap()
+	print("Enter q to quit")
+	i=input("Enter the value: ")
+	while i!='q':
+		H.insertKey(i)
+		i=input('Enter the value: ')
+	search=input('Enter the search element: ')
+	H.search(search)
+	H.keys()
 
 
 if __name__ == '__main__':
-    main()
+	main()
 
